d86d6a5e88f3099e4b861589f3f2e2645b06f000
wei qi <weiqi@hzdracom.com>
2016-09-29 20:59:00 +0800

    Initial Commit

---
+++ AFNetworking3UploadFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworking3UploadFile
+//
+//  Created by 魏琦 on 16/9/29.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNetworking3UploadFile/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AFNetworking3UploadFile
+//
+//  Created by 魏琦 on 16/9/29.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
